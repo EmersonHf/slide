30f85e7f1da0d7fe35e6c39eaf955ec6005beca4
Emerson Silva Higino Filho <emersonfilho1@hotmail.com>
2022-09-05 15:31:43 -0300

    efeito de transição entre slides

--- js/slide.js
+++ js/slide.js
@@ -5,7 +5,18 @@ export default class Slide {
         this.dist = {finalPosition: 0, startX:0, movement: 0}
 
     }
-    
+
+    bindEvents(){
+        this.onStart = this.onStart.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+        this.onMove = this.onMove.bind(this);
+    }
+        
+transition(active) {
+    this.slide.style.transition = active ? 'transform .3s' : '';
+}
+
+
 moveSlide(distX){
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -29,7 +40,7 @@ onStart(event){
 
     }
       this.wrapper.addEventListener(movetype, this.onMove);
-
+    this.transition(false);
 }
 onMove(event){ 
     const pointerPosition = (event.type === 'mousemove') 
@@ -43,6 +54,20 @@ onEnd(event){
     const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
     this.wrapper.removeEventListener('mousemove', this.onMove)
     this.dist.finalPosition = this.dist.movePosition;
+    this.changeSlideOnEnd();
+    this.transition(true);
+}
+
+
+changeSlideOnEnd(){
+    if(this.dist.movement > 120 && this.index.next !== undefined) {
+        this.activeNextSlide();
+    }else if (this.dist.movement < -120 && this.index.prev !== undefined){
+        this.activePrevSlide();
+    } else {
+        this.changeSlide(this.index.active);
+    }
+    console.log(this.dist.movement); 
 }
 
 addSlideEvents(){
@@ -83,16 +108,20 @@ changeSlide(index){
     console.log(this.index);
 }
 
-
-bindEvents(){
-    this.onStart = this.onStart.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-    this.onMove = this.onMove.bind(this);
+activePrevSlide(){
+    if (this.index.prev !== undefined){this.changeSlide(this.index.prev)
 }
+}
+activeNextSlide(){
+    if (this.index.next !== undefined){this.changeSlide(this.index.next)
+}
+}
+
 init(){
     this.bindEvents();
     this.addSlideEvents();
     this.slidesConfig();
+    this.transition(true);
 return this;
 }
 }
